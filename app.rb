bfd681bdf8c9c492182d7167bb72244f80fcf324
Yuliana Alvarado <yalvarado924@gmail.com>
2021-02-10 16:50:24 -0600

    commit tree 5d76a6edb7ac1c2816f66948eedf24ce910a7f7c

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :secret_session, "secret"
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        session[:item] = params[:item]
+        @session = session
+
+        erb :checkout
+    end
 end
